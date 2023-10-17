9502c3ded83bdedd0f22d9207ca5b38da8694789
Lindo <86679153+Lindo-code@users.noreply.github.com>
2023-10-17 18:37:23 +0200

    Update preorderTraversal.js
    updated

--- binary_trees/preorderTraversal.js
+++ binary_trees/preorderTraversal.js
@@ -1,1 +1,32 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
 
+const root = new Node(1);
+const num1 = new Node(6);
+const num2 = new Node(2);
+const num3 = new Node(5);
+
+root.right = num1;              
+num1.left = num2;
+num2.right = num3;
+
+function preorderTraversal(node){
+    const stack = [];
+    const result = [];
+    if(node) stack.push(node)
+    while (stack.length > 0) {
+        const currentNode = stack.pop();
+        console.log(currentNode.value)
+        result.push(currentNode.value)
+        if(currentNode.right) stack.push(currentNode.right);
+        if(currentNode.left) stack.push(currentNode.left);
+
+    }
+    return result;
+}
+preorderTraversal(root)
